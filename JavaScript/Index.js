8e823d83a7864d7ba96ee4f4c15e38f6f5ae4f53
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-01-31 21:28:34 -0500

    finalizada seccion de proyectos

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -5,5 +5,16 @@ filterContainer.addEventListener("click", (event) => {
     if(event.target.classList.contains("filter-item")){
         filterContainer.querySelector(".active").classList.remove("active");
         event.target.classList.add("active")
+        const filterValue = event.target.getAttribute("data-filter")
+        galleryItems.forEach(item => {
+            if(item.classList.contains(filterValue) || filterValue == "all"){
+                item.classList.remove("hide")
+                item.classList.add("show")
+            }
+            else{
+                item.classList.remove("show")
+                item.classList.add("hide")
+            }
+        })
     }
 })
